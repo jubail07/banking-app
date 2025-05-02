439117d1119c24c29602800649082e1fa5bb7cec
Jubail <ibnujubail2003@gmail.com>
2025-05-02 12:22:41 +0530

    fourth commit

--- controller/adminPanel.js
+++ controller/adminPanel.js
@@ -1,27 +1,6 @@
 const Admin = require('../model/Admin')
 const createJWt = require('../utils/adminJWT')
 
-// exports.getSignupPage = async (req, res) => {
-//     return res.render('adminPanel/signup', { msg: '' })
-// }
-
-// exports.signup = async (req, res) => {
-//     try {
-//         let existUser = await Admin.findOne({ username: req.body.username })
-//         if (existUser) {
-//             return res.render('adminPanel/signup', { msg: 'user already exist' })
-//         }
-//         let adminDetails = {
-//             username: req.body.username,
-//             password: req.body.password
-//         }
-//         await Admin.create(adminDetails)
-//         return res.redirect('/login')
-//     } catch (error) {
-//         console.log('error in admin signup')
-//         return res.status(404)
-//     }
-// }
 
 exports.getLoginPage = async (req, res) => {
     return res.render('adminPanel/login', { msg: '' })
